8d9ae89ff9cd9ac1c7a219f0682f33ed6a66e8a0
SocialBureauWeb <web@socialbureau.in>
2025-06-30 12:47:59 +0530

    Final changes About Us page

--- src/components/AboutFooter.jsx
+++ src/components/AboutFooter.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 export default function AboutFooter() {
   return (
-    <section className="max-w-xl mx-auto py-20 px-10 pb-30">
-      <h1 className="text-3xl font-bold mb-3">
+    <section className="max-w-xl mx-auto py-20 px-6">
+      <h1 className="text-3xl sm:text-4xl font-bold mb-4">
         Let’s Build a System That Wins.
       </h1>
-      <p className="mb-7 text-base">
+      <p className="mb-8 text-base sm:text-lg">
         Join us in creating a winning marketing ecosystem tailored to your unique business needs.
       </p>
-      <div className="flex gap-3">
+      <div className="flex flex-col sm:flex-row gap-4">
         <a
           href="#meet-the-team"
-          className="bg-black text-white border font-medium px-6 py-2 sm:px-4 rounded-sm transition-colors hover:bg-[#ff0000]"
+          className="bg-black text-white border border-white font-medium px-6 py-3 rounded-sm transition-colors hover:bg-[#ff0000]"
         >
           Meet the Team
         </a>
         <a
           href="#schedule-discovery"
-          className="bg-white text-black font-medium px-6 py-2 sm:px-4 rounded-sm border-2 border-gray-300 transition-colors hover:border-[#ff0000] hover:text-[#ff0000]"
+          className="bg-white text-black font-medium px-6 py-3 rounded-sm border border-gray-300 transition-colors hover:border-[#ff0000] hover:text-[#ff0000]"
         >
           Schedule a Discovery Call
         </a>
